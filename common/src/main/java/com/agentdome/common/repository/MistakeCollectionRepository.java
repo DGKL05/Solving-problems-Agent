20d217b880316708c693c5735c71e55a30f41891
Huang Wenle <dgklt05t@gmail.com>
2026-06-15 16:02:00 +0800

    feat: add paged mistake repository methods

--- common/src/main/java/com/agentdome/common/repository/MistakeCollectionRepository.java
+++ common/src/main/java/com/agentdome/common/repository/MistakeCollectionRepository.java
@@ -1,12 +1,15 @@
 package com.agentdome.common.repository;
 
 import com.agentdome.common.entity.MistakeCollection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MistakeCollectionRepository extends JpaRepository<MistakeCollection, Long> {
     List<MistakeCollection> findByUserIdOrderByCreatedAtDesc(Long userId);
+    Page<MistakeCollection> findByUserId(Long userId, Pageable pageable);
     List<MistakeCollection> findByUserIdAndCreatedAtBetween(Long userId, LocalDateTime start, LocalDateTime end);
     void deleteByUserId(Long userId);
 }
