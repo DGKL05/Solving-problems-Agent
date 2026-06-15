b096423c0eb1c6415873b9f55f42aea26bbd2438
Huang Wenle <dgklt05t@gmail.com>
2026-06-15 16:01:52 +0800

    feat: add paged problem repository methods

--- common/src/main/java/com/agentdome/common/repository/ProblemRepository.java
+++ common/src/main/java/com/agentdome/common/repository/ProblemRepository.java
@@ -2,10 +2,14 @@ package com.agentdome.common.repository;
 
 import com.agentdome.common.entity.Problem;
 import com.agentdome.common.entity.Problem.SubjectType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
     List<Problem> findByUserIdAndSubjectType(Long userId, SubjectType subjectType);
     List<Problem> findByUserIdOrderByCreatedAtDesc(Long userId);
+    Page<Problem> findByUserId(Long userId, Pageable pageable);
+    Page<Problem> findByUserIdAndSubjectType(Long userId, SubjectType subjectType, Pageable pageable);
 }
